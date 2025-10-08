54224133b46d5ea9bfb9d07a51ce4e768de0491d
235005 <235005@KB37-303-C9>
2025-10-08 11:45:03 +0300

    создал перечисление жанров

--- pr4/Program.cs
+++ pr4/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryManagement
+{
+    // COMMIT: СОЗДАНИЕ ПЕРЕЧИСЛЕНИЯ ЖАНРОВ
+    // Добавляем enum Genre с тремя вариантами жанров для книг
+    public enum Genre
+    {
+        Thriller,
+        Detetive,
+        Action
+    }
+}
