86410634c8b71044c9e2db6b00075ae0636884dc
235005 <235005@KB37-303-C9>
2025-10-08 11:50:13 +0300

    создали класс Book

--- pr4/Program.cs
+++ pr4/Program.cs
@@ -12,4 +12,15 @@ namespace LibraryManagement
         Detetive,
         Action
     }
+    // Создаем класс Book, со всеми данными
+    public class Book
+    {
+        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
 }
